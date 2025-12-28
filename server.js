4f1907b78a9e062c1b580e6c093293f45d398c1e
Rahman <rahmnakhanrajak@gmail.com>
2025-12-28 18:27:19 +0530

    static server1

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const PORT = process.env.PORT || 10000;
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port", PORT);
+  console.log(`Server running on port ${PORT}`);
 });
